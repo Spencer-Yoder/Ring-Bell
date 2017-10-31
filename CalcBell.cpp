244799620b7031bc2312bed3a1d95dceb77f7bc5
Spencer Yoder <sryoder8@yahoo.com>
2017-10-30 19:49:21 -0500

    Added Function to calculate when to ring the bell

---
+++ CalcBell.cpp
@@ -0,0 +1,28 @@
+#include "Header.h"
+
+void RingBell::CalcTime()
+{
+	int count = 1;
+
+	for (vector<int>::iterator iter = ring_times.begin(); iter != ring_times.end(); iter++)	
+	{
+		int Temp_hour = 0;
+		int Temp_min = 0;
+
+		Temp_hour = current_hour;
+		Temp_min = current_min + *iter;
+
+		while (Temp_min > 60)	//If more then 60 min move back to 0
+		{
+			Temp_min = Temp_min - 60;
+			Temp_hour++;
+		}
+
+		if (Temp_hour > 12)	//If it is more then 12 hours move back to 0
+			Temp_hour = Temp_hour - 12;
+
+		cout << count << "\t" << Temp_hour << ":" << setfill('0') << setw(2) << Temp_min << endl; //output
+		count++;
+	}
+	cout << endl;
+}
